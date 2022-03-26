211fe7298dea7559052425d55ac5b5268aef4000
Villinger <80601981+MrWitzbold@users.noreply.github.com>
2022-03-26 20:24:43 -0300

    Added codon table arrays

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
     {
         string command;
         cout << "\n\nPress 1 to convert dna.txt into rna.txt\n: ";
+        cout << "Press 2 to generate report.txt on amino-acids (needs rna.txt)\n: ";
         cin >> command;
 
         if(command == "1") // CONVERT DNA INTO RNA
@@ -48,6 +49,11 @@ int main()
             rna_file.open("rna.txt");
             rna_file << rna;
             rna_file.close();
+        }
+        else if(command == "2") // GENERATE AMINO-ACID PERCENTAGES
+        {
+            string amino_acids[28] = {"Phenylysine", "Leucine", "Isoleucine", "Methionine", "Valine", "Serine", "Threonine", "Alanine", "Tyrosine", "Tryptophan", "Histidine", "Glutamine", "Asparagine", "Lysine", "Aspartic acid", "Glutamic acid", "Arginine", "Cysteine", "Proline", "Glutamine", "Arginine", "Histidine", "Glutamine", "Glutamine", "Glutamine", "Glutamine", "Glutamine", "Glutamine"};
+            string amino_acid_codons[25][6] = {{"UUU", "UUC", "", "", "", ""}, {"UUA", "UUG", "CUU", "CUC", "CUA", "CUG"}, {"AUU", "AUC", "AUA", "", "", ""}, {"GUU", "GUC", "GUA", "GUG", "", ""}, {"UCU", "UCC", "UCA", "UCG", "AGU", "AGC"}, {"CCU", "CCC", "CCA", "CCG", "", ""}, {"ACU", "ACC", "ACA", "ACG", "", ""}, {"GCU", "GCC", "GCA", "GCG", "", ""}, {"UAU", "UAC", "", "", "", ""}, {"CAU", "CAC", "", "", "", ""}, {"AAU", "AAC", "", "", "", ""}, {"GAU", "GAC", "", "", "", ""}, {"UGU", "UGC", "", "", "", ""}, {"CGU", "CGC", "CGA", "CGG", "AGA", "AGG"}, {"GGU", "GGC", "GGA", "GGG", "", ""}, {"UAA", "UAG", "UGA", "", "", ""}, {"CAA", "CAG", "", "", "", ""}, {"AAA", "AAG", "", "", "", ""}, {"GAA", "GAG", "", "", ""}, {"CGU", "CGC", "CGA", "CGG", "AGA", "AGG"}, {"GGU", "GGC", "GGA", "GGG", "", ""}, {"GGU", "GGC", "GGA", "GGG", "", ""}, {"GGU", "GGC", "GGA", "GGG", "", ""}, {"GGU", "GGC", "GGA", "GGG", "", ""}, {"GGU", "GGC", "GGA", "GGG", "", ""}};
         }
     }
 }
